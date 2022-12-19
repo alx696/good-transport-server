8849df54e57ff875fa54ee98443425cf35df82ea
李小明 <alx696@gmail.com>
2022-12-19 11:18:53 +0800

    增加部分平台32位支持

--- build.sh
+++ build.sh
@@ -6,17 +6,25 @@ go mod download
 
 mkdir -p build
 
-echo "开始构建Linux"
-go build -o "build/gts-amd64"
-echo "完成构建Linux"
+echo "开始构建Linux 64位"
+go build -o "build/gts-linux-64"
+echo "完成构建Linux 64位"
 
-echo "开始构建Windows"
-GOOS=windows go build -o "build/gts-amd64.exe"
-echo "完成构建Windows"
+echo "开始构建Linux 32位"
+GOARCH=386 go build -o "build/gts-linux-32"
+echo "完成构建Linux 32位"
+
+echo "开始构建Windows 64位"
+GOOS=windows go build -o "build/gts-windows-64.exe"
+echo "完成构建Windows 64位"
 
 echo "开始构建Android ARM64"
 GOOS=android GOARCH=arm64 go build -o "build/gts-android_arm64"
 echo "完成构建Android ARM64"
+
+echo "开始构建Android ARM32"
+GOOS=linux GOARCH=arm go build -o "build/gts-android_arm32"
+echo "完成构建Android ARM32"
 
 echo "开始构建Android AAR"
 echo "NDK路径: ${ANDROID_NDK_HOME}"
